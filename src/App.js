7ae11c55df611f7fe51769ce280a7cfd8da8ec48
PriyankaDhuri2501 <priyanka.s238257205@vcet.edu.in>
2024-04-12 01:46:04 +0530

    first added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./components/pages/Home";
+import Albums from "./components/pages/Album.jsx";
+import About from "./components/pages/About.jsx";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+      <Route path="/Album" element={<Albums />} />
+        <Route path="/About" element={<About />} />
+        <Route exact path="/" element={<Home />} />
+        
+        
+      </Routes>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Layout />
+  );
+};
+
+export default App;
